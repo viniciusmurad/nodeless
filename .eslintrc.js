ed605a25493b00c83e0bfcacb563d6fb16e2d64b
Vinicius <vm.murad@gmail.com>
2020-05-11 15:19:08 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
